74da313d54d6af104f0135fd7ea37a67b40f0746
Lautaro Nahuel Romero Collar <lromerocollar@gmail.com>
2022-11-29 16:28:37 -0300

    Division de componentes

--- Front_end_argentina_programa/src/app/app.module.ts
+++ Front_end_argentina_programa/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { AcercaComponent } from './componentes/acerca/acerca.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    NavbarComponent,
+    AcercaComponent,
+    ProyectosComponent,
+    EducacionComponent,
+    SkillsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
